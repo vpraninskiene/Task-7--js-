255a633571539ad8100b08a2058e44c669ccadc5
vpraninskiene <valentina.praninskiene@gmail.com>
2023-08-17 06:53:18 +0300

    init

---
+++ script.js
@@ -0,0 +1,20 @@
+/* ------------------------------ TASK 7 -----------------------------------
+Turimas "audi" objektas.
+
+Parašykite funkciją showObjectKeys, kuri kaip argumentą priims objectą 
+ir grąžins visus jo "key" masyve.
+-------------------------------------------------------------------------- */
+const audi = {
+  make: 'audi',
+  model: 'A6',
+  year: 2005,
+  color: 'white',
+};
+
+
+const showObjectKeys = (object) => {
+  const keys = Object.keys(object);
+  console.log(keys); 
+};
+
+showObjectKeys(audi);
